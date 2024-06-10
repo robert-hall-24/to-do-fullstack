aff34d3d74918aaa1b1c7acce7b78e1a380591e8
Robert Hall <rhall477@gmail.com>
2024-06-10 15:20:17 +1200

    creating docker app for deploying

--- client/apis/add-tasks.ts
+++ client/apis/add-tasks.ts
@@ -1,7 +1,8 @@
 import request from 'superagent'
+import { TaskData } from '../../models/tasks'
 const rootUrl = '/api/v1/tasks/'
 
-export async function postTask(task: string) {
+export async function postTask(task: TaskData) {
   console.log('Frontend task post:', task)
   await request.post(rootUrl).send(task)
 }
